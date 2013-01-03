f4fd8b4e5e046a023ed829511360e6427e79a259
Jarek7 <jarek.jezak@gmail.com>
2013-01-03 12:55:48 +0000

    Initial Project

---
+++ SpudNik/SpudNik/SpudNik/GameEnt.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+
+namespace SpudNik
+{
+    public abstract class GameEnt
+    {
+        // Stuf for models 
+        public String modelname;
+        public Model model = null;
+        
+        //stuff foir physx
+        public Vector3 pos;
+        public Vector3 velocity;
+        public float mass;
+        
+        // for quaternion
+        public Quaternion q;
+        
+        //Stuff for camra
+        public Vector3 cRight;
+        public Vector3 cUp;
+        public Vector3 cLook;
+
+
+        public Matrix worldTransform;
+        public Matrix locilTransform;
+    }
+    public virtual void LoadContent()
+    {
+    model = LoadContent;
+    {
+
+    dsfgjsdfkgjhsdfkghadktghasfgsd;
+
+}
